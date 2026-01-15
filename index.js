c68ca1bef3d37e12dca033d9aa1732131560cb73
devansh05 <devansh2393@gmail.com>
2026-01-15 21:47:37 +0530

    express basics sample

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const express = require("express");
-
+const booksArr = require("./mock-books.json");
 const app = express();
 
-app.get("/", (req, res) => {
-  console.log("🟡🟡🟡 LOG 🟡🟡🟡 →  - Method ", req.method);
-  res.status(200).send("Home");
+//middleware to extract request body in json
+app.use(express.json());
+
+app.get("/books", (req, res) => {
+  res.status(200).json(booksArr);
 });
 
-app.post("/", (req, res) => {
-  console.log("🟡🟡🟡 LOG 🟡🟡🟡 →  - Method ", req.method);
-  res.status(201).send("Post added");
+app.get("/books/:id", (req, res) => {
+  const book = booksArr.find((book) => `${book.id}` === `${req.params.id}`);
+  book?.id
+    ? res.status(200).json(book)
+    : res.status(400).json({ error: "No book found." });
+});
+
+app.post("/book", (req, res) => {
+  const { title, author } = req.body;
+  const books = [
+    ...booksArr,
+    { id: booksArr[booksArr.length - 1].id + 1, title: title, author: author },
+  ];
+
+  res.status(201).json(books);
+});
+
+// app.delete("/delete/book/:id", (req, res) => `${book.id}` !== `${req.params.id}`);
+app.delete("/delete/books/:id", (req, res) => {
+  res
+    .status(200)
+    .json(booksArr.filter((book) => `${book.id}` !== `${req.params.id}`));
 });
 
 app.listen(3000, console.log("🟡🟡🟡 LOG 🟡🟡🟡 →  - LISTENING ON 3000"));
